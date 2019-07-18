53976105253ec5e321c0ed2af51fc97be8460a9c
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-18 03:50:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma
-  if "I LOVE YOU GRANDMA!"
-  else "I LOVE YOU TOO PUMPKIN!"
+  if puts "I LOVE YOU GRANDMA!"
+  else 
+    puts "I LOVE YOU TOO PUMPKIN!"
 end 
 end
 # Whatever you say to grandma, she should respond with
